cc0d61cd0b66ecf7e062d652f6d3520c29d69bf0
Ritik Gupta <ritikgupta9414@gmail.com>
2021-12-07 01:21:58 +0530

    Connect nodejs app to mongodb

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 //This will be the server setup
 
-var express = require('express');
-var app = express();
+let express = require("express");
+let app = express();
 
-app.get('/', function (req, res) {
-  res.status(200).send('Hello World!');
+app.get("/", function (req, res) {
+  res.status(200).send("Hello World!");
 });
 
-var port = process.env.PORT || 3000;
+let port = process.env.PORT || 3000;
 
-var server = app.listen(port, function() {
-  console.log('Express server listening on port ' + port);
-});
+//Adding Mongo db url
+let MONGO_URL =
+  "mongodb+srv://ccbooking:ccbookingsystem@cluster0.hyyed.mongodb.net/CC Booking System?retryWrites=true&w=majority";
 
+//Connected server to mongodb  
+const mongoose = require("mongoose");
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    app.listen(port);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
