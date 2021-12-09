2c67eb69d36cb31561d1c42a1d59560a1a52282b
Rishvic Pushpakaran <rishvic@gmail.com>
2021-12-09 20:11:39 +0530

    Fixed merge conflict on app.js, and formatted files

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 const express = require("express");
 const app = express();
 
-<<<<<<< HEAD
-module.exports = {app}
-
-app.use
-=======
 module.exports = { app };
->>>>>>> f91c546263ff70d864a4cfb318d359acbdf6695a
