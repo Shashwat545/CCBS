bde11c56b402bcfe7516b1f561877e6fec0d1264
pksingh21 <91261208+pksingh21@users.noreply.github.com>
2021-12-18 18:10:37 +0530

    Updated Index.js
    Fixed the bug of vanishing input due to 2 different form renders

--- client/src/views/dashboard/Default/index.js
+++ client/src/views/dashboard/Default/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-
+import NewForm from './NewForm';
 import { Grid } from '@mui/material';
 import { gridSpacing } from 'store/constant';
 import Form from './Form';
@@ -11,18 +11,9 @@ const Dashboard = () => {
     }, []);
 
     return (
-        <Grid container spacing={gridSpacing}>
-            <DeviceIdentifier isMobile={true} isTablet={true}>
-                <Grid item xs={12}>
-                    <Form />
-                </Grid>
-            </DeviceIdentifier>
-            <DeviceIdentifier isDesktop={true}>
-                <Grid item xs={6}>
-                    <Form />
-                </Grid>
-            </DeviceIdentifier>
-        </Grid>
+        <>
+            <Form />
+        </>
     );
 };
 
