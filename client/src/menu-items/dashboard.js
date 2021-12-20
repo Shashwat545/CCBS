55be931c1b379d4088e8517d8d86fff4edbb6982
pksingh21 <91261208+pksingh21@users.noreply.github.com>
2021-12-20 14:49:23 +0530

    Updated DashBoard for specific purposes

--- client/src/menu-items/dashboard.js
+++ client/src/menu-items/dashboard.js
@@ -3,46 +3,34 @@ import { IconDashboard } from '@tabler/icons';
 
 // constant
 const icons = { IconDashboard };
-
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 // ==============================|| DASHBOARD MENU ITEMS ||============================== //
 const Commie = 'Community Centre';
-const Bookie = 'Booking System';
+const Bookie = 'Booker';
 const dashboard = {
     id: 'dashboard',
     title: ` ${Commie}  
              ${Bookie}`,
     type: 'group',
     children: [
+        { id: 'Explore', title: 'User Profile', type: 'item', url: '/dashboard/default', icon: ArrowForwardIosIcon, breadcrumbs: false },
         {
-            id: 'Explore',
-            title: 'Page no. 1',
-            type: 'collapse',
-            url: '/dashboard/default',
-            icon: icons.IconDashboard,
-            breadcrumbs: false,
-            children: [
-                {
-                    id: 'Page no. 2',
-                    title: 'Page no. 2',
-                    type: 'item',
-                    url: '',
-                    target: true
-                },
-                {
-                    id: 'Page no. 3',
-                    title: 'Page no. 3',
-                    type: 'item',
-                    url: '',
-                    target: true
-                },
-                {
-                    id: 'Page no. 4',
-                    title: 'Page no. 4',
-                    type: 'item',
-                    url: '',
-                    target: true
-                }
-            ]
+            id: 'Slot Booking',
+            title: 'Slot Booking',
+            type: 'item',
+            url: '',
+            target: true,
+            icon: ArrowForwardIosIcon,
+            breadcrumbs: false
+        },
+        {
+            id: 'Pending Approvals',
+            title: 'Pending Approvals',
+            type: 'item',
+            url: '',
+            target: true,
+            icon: ArrowForwardIosIcon,
+            breadcrumbs: false
         }
     ]
 };
