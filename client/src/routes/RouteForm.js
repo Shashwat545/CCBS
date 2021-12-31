7e8ed40bd8b8fa048bc7f8e1736928255da67ce3
pksingh21 <91261208+pksingh21@users.noreply.github.com>
2021-12-31 12:53:38 +0530

    Added a router for Form

---
+++ client/src/routes/RouteForm.js
@@ -0,0 +1,23 @@
+import { lazy } from 'react';
+
+// project imports
+import MainLayout from 'layout/MainLayout';
+import Loadable from 'ui-component/Loadable';
+
+// profile page routing
+const FormPage = Loadable(lazy(() => import('views/dashboard/Default/Form')));
+
+// ==============================|| PROFILE PAGE ROUTING ||============================== //
+
+const FormPageRoutes = {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+        {
+            path: '/pages/form-registration',
+            element: <FormPage />
+        }
+    ]
+};
+
+export default FormPageRoutes;
