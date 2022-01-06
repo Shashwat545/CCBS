55513314f940d7c49336913b387d6026e1cd5168
Ritik Gupta <ritikgupta9414@gmail.com>
2022-01-06 17:02:44 +0530

    Router and controller for user added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+
+router.post('/',userController.postCreateUser);
+router.get("/getUser/:userId", userController.getOneUser);
+router.get("/allUsers", userController.getAllUser);
+router.patch("/editPorfile/:userId", userController.updateUserInfo);
+
+module.exports = router;
